6338a962834367b794c11978776a029f5f20356f
Carlos Reyes <reyesrico@hotmail.com>
2020-04-19 01:48:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/stuffie/precache-manifest.f342eaf6d666f6cbb7568cb6d3189382.js"
+  "/StuffieReact/precache-manifest.701b34d10511c60a2c5d88d42c068a30.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/stuffie/index.html", {
+workbox.routing.registerNavigationRoute("/StuffieReact/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
