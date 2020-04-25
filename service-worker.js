e52b28479b6910a14a40c2873bb35a6e0da054b7
Carlos Reyes <reyesrico@hotmail.com>
2020-04-25 02:43:54 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/StuffieReact/precache-manifest.1da80140ecd261c88b615217d5e85a64.js"
+  "/StuffieReact/precache-manifest.ccea515881929c1cd68f7d3130c7c02b.js"
 );
 
 workbox.clientsClaim();
