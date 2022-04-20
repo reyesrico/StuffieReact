3fbb41aa32bbada43d41b3ae112c89f9c9c5f812
Carlos Reyes <creyes@castlighthealth.com>
2022-04-19 17:37:22 -0700

    Fix Product rendering (params and Products).

--- src/components/content/Product.tsx
+++ src/components/content/Product.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { find } from 'lodash';
+import { useParams } from 'react-router-dom';
+import { find, isEmpty } from 'lodash';
 
 import Button from '../shared/Button';
 import Loading from '../shared/Loading';
@@ -9,32 +10,51 @@ import State from '../../redux/State';
 import TextField from '../shared/TextField';
 import WarningMessage from '../shared/WarningMessage';
 import { WarningMessageType } from '../shared/types';
+import { getProductFromProducts } from '../helpers/StuffHelper';
 import { updateProductHook } from '../../redux/products/actions';
 
 import './Product.scss';
 
 const Product = (props: any) => {
-  const dispatch = useDispatch();
   const { hideOfferButton, product, showCost } = props;
+  const { id } = useParams();
+  const dispatch = useDispatch();
   let categories = useSelector((state: State) => state.categories);
-  // let products = useSelector((state: State) => state.products);
   let subcategories = useSelector((state: State) => state.subcategories);
+  let products = useSelector((state: State) => state.products);
   let user = useSelector((state: State) => state.user);
-  // let [id, setId] = useState(null);
   let [cost, setCost] = useState(0.0);
   let [message, setMessage] = useState('');
   let [type, setType] = useState(WarningMessageType.EMPTY);
+  let [category, setCategory] = useState<any>(null);
+  let [subcategory, setSubcategory] = useState<any>(null);
+  let [productRendered, setProductRendered] = useState<any>(null);
+
+  useEffect(() => {
+    if (!isEmpty(products) && !productRendered) {
+      let idP = id ? parseInt(id) : product ? product.id : NaN;
+      let pRendered = getProductFromProducts(idP, products);
+      setProductRendered(pRendered);
+    }
+
+    if (!isEmpty(products) && productRendered) {
+      let cat = find(categories, c => c.id === productRendered.category);
+      setCategory(cat);
+      let subcat = find(subcategories, s => s.id === productRendered.subcategory);
+      setSubcategory(subcat);
+    }
+  }, [categories, subcategories, productRendered, products, product, id]);
 
   const updateCost = (clear: boolean = false) => {
     const updatedCost = clear ? 0 : cost;
-    updateProductHook(user.id, product?.id, updatedCost, dispatch, setMessage, setType)
+    updateProductHook(user.id, productRendered?.id, updatedCost, dispatch, setMessage, setType)
   }
 
   const renderCost = () => {
     if (product?.cost)
       return (
       <div className="product__cost">
-        <div className="product__cost-value">Cost ${product?.cost}</div>
+        <div className="product__cost-value">Cost ${productRendered?.cost}</div>
         {!hideOfferButton && (<div className="product__cost-button">
             <Button text="Stop Offer" onClick={() => updateCost(true)} />
         </div>)}
@@ -52,20 +72,17 @@ const Product = (props: any) => {
     }
   }
 
-  if (!product) return <Loading size="lg" message="Loading product..." />;
-
-  const category = find(categories, c => c.id === product.category);
-  const subcategory = find(subcategories, s => s.id === product.subcategory);
+  if (!productRendered) return <Loading size="lg" message="Loading product..." />;
 
   return (
     <div className="product">
       <WarningMessage message={message} type={type} />
-      <h3>{ product.name }</h3>
+      <h3>{ productRendered.name }</h3>
       <hr />
         <Media
-          fileName={product.id}
-          category={product.category}
-          subcategory={product.subcategory}
+          fileName={productRendered.id}
+          category={productRendered.category}
+          subcategory={productRendered.subcategory}
           isProduct="true"
           height="200"
           width="100" />
