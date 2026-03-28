dfe655db76f55e60680dc477e29973a24648b981
Carlos Reyes <reyesrico@hotmail.com>
2026-03-27 23:00:04 -0700

    Updates

---
+++ assets/Support-EaxPTsyz.js
@@ -0,0 +1,2 @@
+import{j as t}from"./vendor-fluent-DaIvjl3u.js";import"./vendor-react-oEplC0Kt.js";const o=()=>t.jsx("iframe",{src:"https://webchat.botframework.com/embed/StuffieBot?s=fkpQavtTB78.cwA.pjM.9djZJuhRob4Mt-Lb5-1Wk7yrsHtLAxOKIL6w3gE4910",title:"WebChat"});export{o as default};
+//# sourceMappingURL=Support-EaxPTsyz.js.map
